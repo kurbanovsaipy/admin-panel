e3e689c8fffe722dc67e44673d152f9f3a7f3f70
salimKurbanov <salim.kurbanov08@gmail.com>
2024-09-30 22:13:57 +0300

    upd

--- src/useMain.js
+++ src/useMain.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
+import useMode from "./utils/useMode";
+import Store from "./Store";
 
 
 export default function useMain() {
 
     const [load, setLoad] = useState(false)
 
-    useEffect(() => {
+    const mode = useMode()
 
+    useEffect(() => {
+        
         setTimeout(() => {
             setLoad(true)
         }, 3100)
 
+        setTimeout(() => {
+            Store.setListener('start_mode', mode)
+        }, 3200)
+
     }, [])
 
-    return { load }
+    return { load, mode }
 }
--- src/utils/useMode.js
+++ src/utils/useMode.js
@@ -3,12 +3,21 @@ import Store from "../Store";
 
 export default function useMode() {
 
-    const [mode, setMode] = useState(localStorage.getItem('Mode') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
-    //Store.useListener('mode', setMode)
+    const lightTheme = '--background: #fff'
+    const darkTheme = 'background: #131313'
+
+    const [mode, setMode] = useState(localStorage.getItem('Mode') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+    
+    Store.useListener('mode', setMode)
 
     useEffect(() => {
+        if(mode) {
+            document.body.style = mode === 'dark' ? darkTheme : lightTheme
+        }
+
         const handleChange = (event) => {
             localStorage.removeItem('Mode')
+            Store.setListener('start_mode', event.matches ? 'dark' : 'light')
             setMode(event.matches ? 'dark' : 'light');
         };
 
@@ -17,7 +26,7 @@ export default function useMode() {
         return () => {
             window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleChange);
         };
-    }, []);
+    }, [mode]);
 
-    return [mode];
+    return mode;
 }
